855eec4102199b6a3d1798f0e84569bee22ecba8
Yash priyam <yashpriyam74@gmail.com>
2024-06-20 12:32:12 +0530

    add core-ml sqlize

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { default as sequelize } from './sequelize';
-export * from './models/user.model';
+export { default as portalSequelize } from './portal-sequelize';
+export { default as coreMlSequelize } from './core-ml-sequelize';
+export * from './portal-models';
+export * from './core-ml-models';
