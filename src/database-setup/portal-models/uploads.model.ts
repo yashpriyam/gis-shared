3800422d4d27aee67f93d46902bc8b90a0913ae2
Obliger0 <sujalgotharwal904@gmail.com>
2024-07-01 15:48:56 +0530

    Update Upload table

--- src/database-setup/portal-models/uploads.model.ts
+++ src/database-setup/portal-models/uploads.model.ts
@@ -55,4 +55,64 @@ export class Upload extends Model {
         allowNull: false
     })
     updatedAt!: Date;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    fileName?: string;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    etag!: string;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: true
+    })
+    lastModified!: Date;
+
+    @Column({
+        type: DataType.JSON,
+        allowNull: true,
+    })
+    xMsContentCrc64?: object;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    clientRequestId!: string;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true
+    })
+    requestId!: string;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: false
+    })
+    uploadedFileUrl!: string;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: false
+    })
+    blobName!: string;
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: false
+    })
+    contentType!: string;
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false
+    })
+    version!: Date;
 }
